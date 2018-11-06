be34644dd10872bd0b9beb47bcf27241d75783ba
Unknown <edgar.i840@gmail.com>
2018-11-06 20:32:29 +0100

    Added camera and player controller
    Got to fix the player

---
+++ Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] Camera mainCamera;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.GetComponent<Rigidbody>().AddForce(mainCamera.transform.forward * movementSpeed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.GetComponent<Rigidbody>().AddForce(mainCamera.transform.forward * -movementSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.GetComponent<Rigidbody>().AddForce(mainCamera.transform.right * movementSpeed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.GetComponent<Rigidbody>().AddForce(mainCamera.transform.right * -movementSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/FirstPersonCamera.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    // Mouse values
+    [SerializeField] private float mouseSensitivity = 20;
+    [SerializeField] private float rotationSmoothTime = 0.07f;
+
+    // Min and Max value the player will be able to move the camera
+    [SerializeField] private Vector2 pitchMinMax = new Vector2(-40, 85);    // Not X and Y | Min and Max values
+
+    // Actual yaw and pitch values | Only to debug
+    [SerializeField] private float yaw;
+    [SerializeField] private float pitch;
+
+    // Camera values
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    void Start()
+    {
+        // Lock the mouse in the center and hide it from the player
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // If the cursor is locked
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+            pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+            pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+            currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+            transform.eulerAngles = currentRotation;
+        }
+
+
+        // If we press the Escape key (to show a menu for example) the cursor will be unlocked and will be visible again
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState != CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+    }
+}
