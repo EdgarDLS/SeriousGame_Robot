84ded4cade56a028b77669b563cd0e04bd346ebf
Unknown <edgar.i840@gmail.com>
2018-12-04 21:26:07 +0100

    Isntalled Yarn and Merino for the dialogue system

---
+++ Assets/Scripts/Dialog/Dialog.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog : MonoBehaviour, IInteractuable
+{
+    [SerializeField] private List<string> dialog = new List<string>();
+
+    public void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+}
